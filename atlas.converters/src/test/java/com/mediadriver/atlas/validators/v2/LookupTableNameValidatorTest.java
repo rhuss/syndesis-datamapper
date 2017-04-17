853c20d4c3f1cdda72ac255f0284a9ba6fe72b44
Matt Pavlovich <matt@mediadriver.com>
2017-04-17 12:28:21 -0500

    * Add tests for LookupTable support to converters

---
+++ atlas.converters/src/test/java/com/mediadriver/atlas/validators/v2/LookupTableNameValidatorTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2017 Red Hat, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mediadriver.atlas.validators.v2;
+
+import com.mediadriver.atlas.v2.AtlasModelFactory;
+import com.mediadriver.atlas.v2.LookupTable;
+import com.mediadriver.atlas.v2.LookupTables;
+import com.mediadriver.atlas.validators.v2.core.AtlasMappingErrors;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ */
+public class LookupTableNameValidatorTest extends BaseMappingTest {
+    private LookupTableNameValidator validator;
+
+    @Before
+    public void setUp() throws Exception {
+        validator = new LookupTableNameValidator("lookuptables.lookuptable.name", "LookupTables contain duplicated LookupTable names.");
+    }
+
+    @Test
+    public void supports() throws Exception {
+        LookupTables lookupTables = makeLookupTables();
+        assertTrue(validator.supports(lookupTables.getClass()));
+    }
+
+    @Test
+    public void validate_DuplicatedNames() throws Exception {
+        Errors errors = new AtlasMappingErrors();
+        LookupTables lookupTables = makeLookupTables();
+        validator.validate(lookupTables, errors);
+        assertTrue(errors.hasErrors());
+        debugErrors(errors);
+    }
+
+    @Test
+    public void validate_NoDuplicateNames() throws Exception {
+        Errors errors = new AtlasMappingErrors();
+        LookupTables lookupTables = makeLookupTables();
+        lookupTables.getLookupTable().remove(2);
+        validator.validate(lookupTables, errors);
+        assertFalse(errors.hasErrors());
+    }
+
+
+    private LookupTables makeLookupTables() {
+        LookupTables lookupTables = new LookupTables();
+        LookupTable lookupTable = AtlasModelFactory.createLookupTable();
+        LookupTable lookupTable2 = AtlasModelFactory.createLookupTable();
+        LookupTable lookupTableDup = AtlasModelFactory.createLookupTable();
+        lookupTable.setName("qwerty");
+        lookupTable2.setName("anotherName");
+        lookupTableDup.setName("qwerty");
+        lookupTables.getLookupTable().add(lookupTable);
+        lookupTables.getLookupTable().add(lookupTable2);
+        lookupTables.getLookupTable().add(lookupTableDup);
+        return lookupTables;
+    }
+
+}
